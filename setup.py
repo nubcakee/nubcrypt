970a564617203f31c44dc5d8114b631a1ccd33ee
Anggi Kharisma Putra <anggikharismaputra@gmail.com>
2022-04-19 15:56:39 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension, find_packages
+setup(
+    name="nubcrypt",
+    version="0.0.1",
+    description="Data encryption",
+    author="Anggi Kharisma Putra",
+    author_email="anggikharismaputra@gmail.com",
+    packages = find_packages(),
+    setup_requires=["cython"],
+    ext_modules= [
+        Extension(
+            'nubcrypt._core._core',
+            sources=["nubcrypt/_core/_core.pyx"]
+        )
+    ],
+)
